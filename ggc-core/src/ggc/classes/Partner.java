9b6ead116a8f066aec2229b2f6980cb9fba095f2
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-02-03 22:55:15 +0000

    feat: partner getters and setters

--- ggc-core/src/ggc/classes/Partner.java
+++ ggc-core/src/ggc/classes/Partner.java
@@ -9,5 +9,63 @@ public class Partner {
     private double _valueSales;
     private double _valuePaidSales;
         
-    
+    // Getters Setters
+    public String getPartnerKey(){
+        return _partnerKey;
+    }
+
+    public String getName(){
+        return _name;
+    }
+
+    public String getAdress(){
+        return _adress;
+    }
+
+    public int getPoints(){
+        return _points;
+    }
+
+    public double getValueAcquisitions(){
+        return _valueAcquisitions;
+    }
+
+    public double getValueSales(){
+        return _valueSales;
+    }
+
+    public double getValuePaidSales(){
+        return _valuePaidSales;
+    }
+
+    public void setPartnerKey(String partnerKey){
+        _partnerKey = partnerKey;
+    }
+
+    public void setName(String name){
+        _name = name;
+    }
+
+    public void setAdress(String adress){
+        _adress = adress;
+    }
+
+    public void setPoints(int points){
+        _points = points;
+    }
+
+    public void setValueAcquisitions(double valueAcquisitions){
+        _valueAcquisitions = valueAcquisitions;
+    }
+
+    public void setValueSales(double valueSales){
+        _valueSales = valueSales;
+    }
+
+    public void setValuePaidSales(double valuePaidSales){
+        _valuePaidSales = valuePaidSales;
+    }
+
+    // Methods
+
 }
