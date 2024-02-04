1d758525dc41f4d1185b0559ae5f62bbcb5d1746
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-02-04 16:39:46 +0000

    feat: Partner constructor

--- ggc-core/src/ggc/classes/Partner.java
+++ ggc-core/src/ggc/classes/Partner.java
@@ -3,12 +3,24 @@ package ggc.classes;
 public class Partner {
     private String _partnerKey;
     private String _name;
-    private String _adress;
+    private String _address;
     private int _points;
     private double _valueAcquisitions;
     private double _valueSales;
     private double _valuePaidSales;
-        
+    
+    // Constructor
+
+    public Partner(String partnerKey, String name, String address){
+        _partnerKey = partnerKey;
+        _name = name;
+        _address = address;
+        _points = 0;
+        _valueAcquisitions = 0;
+        _valueSales = 0;
+        _valuePaidSales = 0;
+    }
+
     // Getters Setters
     public String getPartnerKey(){
         return _partnerKey;
@@ -18,8 +30,8 @@ public class Partner {
         return _name;
     }
 
-    public String getAdress(){
-        return _adress;
+    public String getAddress(){
+        return _address;
     }
 
     public int getPoints(){
@@ -46,8 +58,8 @@ public class Partner {
         _name = name;
     }
 
-    public void setAdress(String adress){
-        _adress = adress;
+    public void setAddress(String address){
+        _address = address;
     }
 
     public void setPoints(int points){
@@ -67,5 +79,5 @@ public class Partner {
     }
 
     // Methods
-
+    
 }
