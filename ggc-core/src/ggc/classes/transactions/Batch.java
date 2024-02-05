244ef5f33a1b29ee0de38dac5ceb5c36e67fd2cc
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-02-05 22:24:20 +0000

    feat: batch class

--- ggc-core/src/ggc/classes/transactions/Batch.java
+++ ggc-core/src/ggc/classes/transactions/Batch.java
@@ -1,12 +1,56 @@
 package ggc.classes.transactions;
 
 public class Batch {
-
+    
+    private String _partnerKey;
+    private String _productId;
     private double _price;
     private int _quantity;
 
-    //public String toString(){}
-
-    //protected Batch makeCopy(){}
+    // Constructor
+    public Batch(String partnerKey, String productId, double price, int quantity){
+        _partnerKey = partnerKey;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
     
+    // Getters and Setters
+    public String getPartnerKey(){
+        return _partnerKey;
+    }
+
+    public String getProductId(){
+        return _productId;
+    }
+
+    public double getPrice(){
+        return _price;
+    }
+
+    public int getQuantity(){
+        return _quantity;
+    }
+
+    public void setPartnerKey(String partnerKey){
+        this._partnerKey = partnerKey;
+    }
+
+    public void setProductId(String productId){
+        this._productId = productId;
+    }
+
+    public void setPrice(double price){
+        this._price = price;
+    }
+
+    public void setQuantity(int quantity){
+        this._quantity = quantity;
+    }    
+
+    @Override
+    public String toString() {
+        return  _partnerKey + "| " + _productId + "| " + _price + "| " + _quantity;
+    }
+
 }
