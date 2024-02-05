719eeb48c1f41a22027ad62e82fb9927e645f635
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-02-05 20:07:14 +0000

    feat: product

--- ggc-core/src/ggc/classes/Product.java
+++ ggc-core/src/ggc/classes/Product.java
@@ -5,9 +5,30 @@ abstract class Product{
     private double _maxPrice;
     private String id;
 
-    public void Product(String id){}
+    public void Product(String id){
+        this.id = id;
+        this._maxPrice = 0;
+    }
 
-   // abstract protected void checkQuantity(int quantity, Partner p);
+    // Getters and Setters
+    public double getMaxPrice(){
+        return _maxPrice;
+    }
 
+    public String getId(){
+        return id;
+    }
 
+    public void setMaxPrice(double maxPrice){
+        this._maxPrice = maxPrice;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Product ID: " + id + "| Max Price: " + _maxPrice;
+    }
 }
